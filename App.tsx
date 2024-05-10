153145a7a95562da1baa8f2e0e5f3444c69eac7c
David <debukali@gmail.com>
2024-05-10 14:11:53 +0300

    Setup rtk store

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigation from './features/Navigation/StackNavigation';
+import StackNavigation from './src/features/Navigation/StackNavigation';
+import { store } from './src/app/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigation />
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <StackNavigation />
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </Provider>
   );
 }
