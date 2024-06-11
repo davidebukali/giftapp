f6eadcd7acf9b1b65bbfaa63286c344f41c23d13
David <debukali@gmail.com>
2024-06-11 21:25:37 +0300

    Use a flatlist component for GiftCategories

--- src/features/Gift/GiftCategory.tsx
+++ src/features/Gift/GiftCategory.tsx
@@ -1,69 +1,74 @@
 import {
   Dimensions,
+  FlatList,
   Image,
   Pressable,
+  SafeAreaView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 import uuid from 'react-native-uuid';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb',
+    name: 'Cakes',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    name: 'Flowers',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    name: 'Chocolates',
+  },
+  {
+    id: '58694a0f-3da1-471f-5571e29d72',
+    name: 'Hampers',
+  },
+  {
+    id: '4a0f-3da1-471f-bd96-145571e29d72',
+    name: 'Custom',
+  },
+];
+
 const { width } = Dimensions.get('window');
 const GiftCategory = ({ navigation }) => {
   return (
-    <View style={[styles.container, { width: width }]}>
-      <Pressable onPress={() => navigation.navigate('GiftList')}>
-        <View style={styles.item}>
-          <Image
-            source={require('../../../assets/placeholder.png')}
-            style={styles.image}
-          />
-          <Text style={styles.text}>{'Cakes'}</Text>
-        </View>
-      </Pressable>
-      <View style={styles.item}>
-        <Image
-          source={require('../../../assets/placeholder.png')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>{'Flowers'}</Text>
-      </View>
-      <View style={styles.item}>
-        <Image
-          source={require('../../../assets/placeholder.png')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>{'Chocolates'}</Text>
-      </View>
-      <View style={styles.item}>
-        <Image
-          source={require('../../../assets/placeholder.png')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>{'Hampers'}</Text>
-      </View>
-      <View style={styles.lastItem}>
-        <Image
-          source={require('../../../assets/placeholder.png')}
-          style={styles.image}
-        />
-        <Text style={styles.text}>{'Custom'}</Text>
-      </View>
-      <View style={styles.lastView}></View>
-    </View>
+    <SafeAreaView
+      style={{
+        width: width,
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+      }}
+    >
+      <FlatList
+        numColumns={2}
+        data={DATA}
+        renderItem={({ item }) => {
+          return (
+            <Pressable onPress={() => navigation.navigate('GiftList')}>
+              <View style={styles.item}>
+                <Image
+                  source={require('../../../assets/placeholder.png')}
+                  style={styles.image}
+                />
+                <Text style={styles.text}>{item.name}</Text>
+              </View>
+            </Pressable>
+          );
+        }}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 10,
-    justifyContent: 'space-around',
-    gap: 10,
-  },
   item: {
-    maxWidth: '50%',
+    maxWidth: '100%',
     height: 150,
     padding: 10,
   },
@@ -72,17 +77,6 @@ const styles = StyleSheet.create({
   },
   text: {
     alignContent: 'center',
-  },
-  lastItem: {
-    maxWidth: '50%',
-    height: 150,
-    padding: 10,
-    marginLeft: 5,
-  },
-  lastView: {
-    width: '50%',
-    height: 150,
-    padding: 10,
   },
 });
 
